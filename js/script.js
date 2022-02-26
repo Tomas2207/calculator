fc430680fea8a2f80987d7c11da00467158229d8
Tomas2207 <tomas.abraham@outlook.es>
2022-02-26 02:46:13 -0300

    it just works

--- js/script.js
+++ js/script.js
@@ -14,9 +14,46 @@ buttons.forEach((button) => {
     let a = parseInt(button.id);
 
     if (Number.isNaN(a)) {
-      if (second) {
-        result = operate(number, operator, number2);
+      if (button.id === "ac") {
+        reset();
+        console.log("numbers" + n1 + n2);
+        second = false;
+        result = 0;
         resultContainer.innerHTML = result;
+      }
+      if (button.id === "dot") {
+        if (second === false) {
+          n1 += ".";
+          console.log(n1);
+          resultContainer.innerHTML = Math.round(+number * 100) / 100;
+        } else {
+          n2 += ".";
+          console.log(n2);
+          resultContainer.innerHTML = Math.round(+number2 * 100) / 100;
+        }
+      }
+      if (button.id === "maso") {
+        if (second === false) {
+          if (number > 0) {
+            number = "-" + number;
+            console.log(n1);
+          } else {
+            number = Math.abs(number);
+          }
+          resultContainer.innerHTML = number;
+        } else {
+          if (number2 > 0) {
+            number2 = "-" + number2;
+            console.log(n2);
+          } else {
+            number2 = Math.abs(number2);
+          }
+          resultContainer.innerHTML = number2;
+        }
+      }
+      if (second && button.id !== "dot" && button.id !== "maso") {
+        result = operate(number, operator, number2);
+        resultContainer.innerHTML = Math.round(+result * 100) / 100;
         if (button.id === "=") {
           second = false;
           number = result;
@@ -28,16 +65,19 @@ buttons.forEach((button) => {
         }
 
         console.log(result);
-      } else {
+      } else if (
+        button.id !== "ac" &&
+        (button.id !== "dot") & (button.id !== "maso")
+      ) {
         operator = button.id;
         second = true;
       }
     } else if (second === false) {
       number = numberOne(button.id);
-      resultContainer.innerHTML = number;
+      resultContainer.innerHTML = Math.round(+number * 100) / 100;
     } else if (second) {
       number2 = numberTwo(button.id);
-      resultContainer.innerHTML = number2;
+      resultContainer.innerHTML = Math.round(+number2 * 100) / 100;
     }
     console.log(
       "number: " + number + " operator: " + operator + "otherN: " + number2
@@ -76,9 +116,13 @@ const operate = function (n1, op, n2) {
     case "/":
       result = divide(n1, n2);
       break;
+    case "%":
+      result = remain(n1, n2);
+      break;
 
     default:
-      alert("not valid");
+      // alert("not valid");
+      reset();
   }
   return result;
 };
@@ -101,4 +145,16 @@ const multiply = function (n1, n2) {
 const divide = function (n1, n2) {
   let result = +n1 / +n2;
   return result;
+};
+
+const remain = function (n1, n2) {
+  let result = (n2 * n1) / 100;
+  return result;
+};
+
+const reset = function () {
+  n1 = "";
+  n2 = "";
+  number = 0;
+  number2 = 0;
 };
